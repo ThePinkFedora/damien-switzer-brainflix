8ff1c532743a58a58fe70ae7557f8e92ed796ce5
Damien Switzer <damienswitzer@hotmail.com>
2023-04-04 14:10:10 -0700

    Implement responsive layout matching mockups

--- src/components/CtaButton/CtaButton.js
+++ src/components/CtaButton/CtaButton.js
@@ -1,12 +1,20 @@
 import './CtaButton.scss';
 
-function CtaButton({text="TEXT",iconSrc="",iconAlt=""}){
+/**
+ * 
+ * @param {object} prop
+ * @param {string} props.text - The button text
+ * @param {string} props.iconSrc - The icon source url
+ * @param {string} props.iconAlt - The icon alt text 
+ * @returns 
+ */
+function CtaButton(props){
     return (
         <button className="cta-button">
-            <img className="cta-button__icon" src={iconSrc} alt={iconAlt}/>
-            {text}
+            <img className="cta-button__icon" src={props.iconSrc} alt={props.iconAlt}/>
+            {props.text}
         </button>
     );
 }
 
-export default CtaButton;
+export default CtaButton; 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,22 +3,24 @@ import Logo from "../Logo/Logo";
 import Avatar from "../Avatar/Avatar";
 import SearchBar from "../SearchBar/SearchBar";
 import CtaButton from "../CtaButton/CtaButton";
-import uploadIcon from '../../assets/images/upload.svg';
+import uploadIcon from "../../assets/images/upload.svg";
 
 export default function Header() {
   return (
     <header className="navbar">
-      <div className="navbar__item navbar__item--logo">
-        <Logo />
-      </div>
-      <div className="navbar__item navbar__item--search-bar">
-        <SearchBar />
-      </div>
-      <div className="navbar__item navbar__item--avatar">
-        <Avatar />
-      </div>
-      <div className="navbar__item navbar__item--upload-btn">
-        <CtaButton text="UPLOAD" iconSrc={uploadIcon} iconAlt="upload" />
+      <div class="navbar__content">
+        <div className="navbar__item navbar__item--logo">
+          <Logo />
+        </div>
+        <div className="navbar__item navbar__item--search-bar">
+          <SearchBar />
+        </div>
+        <div className="navbar__item navbar__item--avatar">
+          <Avatar />
+        </div>
+        <div className="navbar__item navbar__item--upload-btn">
+          <CtaButton text="UPLOAD" iconSrc={uploadIcon} iconAlt="upload" />
+        </div>
       </div>
     </header>
   );
