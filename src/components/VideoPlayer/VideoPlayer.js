39007fefccd4464781d87fd37df692d4d2cdbd38
Damien Switzer <damienswitzer@hotmail.com>
2023-04-04 15:09:41 -0700

    correct layout, implement poster

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -1,9 +1,10 @@
 import './VideoPlayer.scss';
+
 function VideoPlayer(){
     return (
         <div className="video-player">
-            <video className="video-player__video" controls>
-            <source src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm" type="video/webm"/>
+            <video className="video-player__video" poster="https://i.imgur.com/l2Xfgpl.jpg" controls>
+            <source type="video/webm"/>
             Your browser does not support the video tag.
             </video>
         </div>
