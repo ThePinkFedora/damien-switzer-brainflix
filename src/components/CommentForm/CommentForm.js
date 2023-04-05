b3ca71cb8c1ff47b9da3aaa9ff4a023b0846a785
Damien Switzer <damienswitzer@hotmail.com>
2023-04-05 16:08:47 -0700

    Implement comment section state and commenting functionality

--- src/components/CommentForm/CommentForm.js
+++ src/components/CommentForm/CommentForm.js
@@ -3,18 +3,32 @@ import CtaButton from "../CtaButton/CtaButton";
 import Avatar from "../Avatar/Avatar";
 import commentIcon from "../../assets/images/add-comment.svg";
 
-function CommentForm() {
+function CommentForm(props) {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if(event.target.comment.value){
+      const name = "USERNAME";
+      const comment = event.target.comment.value;
+      
+      props.onComment(name,comment);
+
+      event.target.reset();
+    }
+  };
+
   return (
     <div className="comment-form">
       <div className="comment-form__avatar-wrapper">
         <Avatar />
       </div>
-      <form className="comment-form__form">
+      <form className="comment-form__form" onSubmit={handleSubmit}>
         <div className="comment-form__item comment-form__item--comment">
-          <label class="comment-form__label" for="comment">
+          <label class="comment-form__label" htmlFor="comment">
             JOIN THE CONVERSATION
           </label>
-          <textarea class="comment-form__textarea" placeholder="Add a new comment"></textarea>
+          <textarea name="comment" class="comment-form__textarea" placeholder="Add a new comment"></textarea>
         </div>
         <div className="comment-form__item comment-form__item--submit">
         <CtaButton text="COMMENT" iconSrc={commentIcon} iconAlt="comment" />
