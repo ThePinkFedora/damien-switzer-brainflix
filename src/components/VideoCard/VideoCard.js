eb6a0e4a5bb5da1f5c56769560fbd6169d64b5ff
Damien Switzer <damienswitzer@hotmail.com>
2023-04-08 11:59:50 -0700

    add animations to video cards and logo

--- src/components/VideoCard/VideoCard.js
+++ src/components/VideoCard/VideoCard.js
@@ -1,17 +1,25 @@
 import './VideoCard.scss';
 
-function VideoCard(props){
-
+/**
+ * 
+ * @param {object} props 
+ * @param {string} props.title
+ * @param {string} props.channel
+ * @param {string} props.image
+ * @param {function} props.image
+ * @returns 
+ */
+function VideoCard({title,channel,image,onClick}){
     const handleClick = () => {
-        props.onClick();
+        onClick();
     };
 
     return (
         <article className="video-card" onClick={handleClick}>
-            <img className="video-card__thumbnail" src={props.image} alt="thumbnail"/>
+            <img className="video-card__thumbnail" src={image} alt="thumbnail"/>
             <div className="video-card__info">
-                <h4 className="video-card__title">{props.title}</h4>
-                <span className="video-card__channel">{props.channel}</span>
+                <h4 className="video-card__title">{title}</h4>
+                <span className="video-card__channel">{channel}</span>
             </div>
         </article>
     );
